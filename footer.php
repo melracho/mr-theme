8199a03833159967663d35a6239fa294350aba7d
Mel Racho <melracho@Mel-Rachos-MacBook-Pro-5.local>
2013-09-04 21:09:38 -0400

    removed GA

--- footer.php
+++ footer.php
@@ -40,16 +40,7 @@
 
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/bootstrap-lightbox.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-41546783-1', 'utoronto.ca');
-  ga('send', 'pageview');
-
-</script>
 
 <?php wp_footer(); ?>
 </body>
